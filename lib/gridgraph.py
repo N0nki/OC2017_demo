5944366d8afb01246b97b1ccedd502c3ebbb56e9
N0nki <rgstr_land@icloud.com>
2017-07-23 23:17:00 +0900

    update

--- lib/gridgraph.py
+++ lib/gridgraph.py
@@ -1,3 +1,5 @@
+import random
+
 import networkx as nx
 import matplotlib.pyplot as plt
 
@@ -6,63 +8,54 @@ from graphillion import GraphSet
 class GridGraph:
 
     def __init__(self, m, n):
-        self.graph = self._grid_graph(m, n)
+        self.m = m
+        self.n = n
+        self.nx_graph = self._grid_graph(m, n)
 
     def _grid_graph(self, m, n):
         """
         m×nの格子グラフを返す
         """
-        m, n = m + 1, n + 1
+        m, n = self.m + 1, self.n + 1
         edges = []
         for v in range(1, m * n + 1):
+            w = random.randint(1, 100)
             if v % n != 0:
-                edges.append((v, v + 1))
+                edges.append((v, v + 1, w))
+            w = random.randint(1, 100)
             if v <= (m - 1) * n:
-                edges.append((v, v + n))
-        return nx.Graph(data=edges)
+                edges.append((v, v + n, w))
+        G = nx.Graph()
+        G.add_weighted_edges_from(edges)
+        return G
 
-    def draw(self, subgraph=None):
+    def draw(self, subgraph=None, edge_labels=None):
         """
         格子グラフを描画する
         """
         universe = GraphSet.universe()
         if not isinstance(universe, nx.Graph):
             universe = nx.Graph(data=list(universe))
-        n = sorted(universe[1].keys())[1] - 1
-        m = universe.number_of_nodes() // n
-        self.graph.add_nodes_from(universe.nodes())
+        self.n = sorted(universe[1].keys())[1] - 1
+        self.m = universe.number_of_nodes() // self.n
+        self.nx_graph.add_nodes_from(universe.nodes())
         pos = {}
-        for v in range(1, m * n + 1):
-            pos[v] = ((v - 1) % n, (m * n - v) // n)
-        nx.draw_networkx_nodes(self.graph, pos, node_color='w')
-        nx.draw_networkx_labels(self.graph, pos)
-        nx.draw_networkx_edges(self.graph, pos)
-        plt.xticks([])
-        plt.yticks([])
-        if subgraph is not None:
+        plt.figure(figsize=(self.m, self.n))
+        for v in range(1, self.m * self.n + 1):
+            pos[v] = ((v - 1) % self.n, (self.m * self.n - v) // self.n)
+        nx.draw_networkx_nodes(self.nx_graph, pos, node_color='w')
+        nx.draw_networkx_labels(self.nx_graph, pos)
+        nx.draw_networkx_edges(self.nx_graph, pos)
+        if edge_labels:
+            nx.draw_networkx_edge_labels(self.nx_graph, pos, edge_labels=edge_labels, font_size=8)
+        if subgraph:
             subgraph = nx.Graph(data=subgraph)
             nx.draw_networkx_nodes(subgraph, pos, node_color='r')
-            nx.draw_networkx_labels(subgraph, pos, font_size=10)
+            # nx.draw_networkx_labels(subgraph, pos, font_size=10)
             nx.draw_networkx_edges(subgraph, pos, edgelist=subgraph.edges(), edge_color='r', width=3.0)
+            if edge_labels:
+                subgraph_metric = {(e[0],e[1]): edge_labels[e] for e in subgraph.edges()}
+                nx.draw_networkx_edge_labels(subgraph, pos, edge_labels=subgraph_metric, font_color='r', font_size=8)
+        plt.xticks([])
+        plt.yticks([])
         plt.show()
-    # def draw_grid_graph(G):
-    #     """
-    #     格子グラフを描画する
-    #     """
-    #     universe = GraphSet.universe()
-    #     if not isinstance(universe, nx.Graph):
-    #         universe = nx.Graph(data=list(universe))
-    #     if not isinstance(G, nx.Graph):
-    #         G = nx.Graph(data=G)
-    #     n = sorted(universe[1].keys())[1] - 1
-    #     m = universe.number_of_nodes() // n
-    #     G.add_nodes_from(universe.nodes())
-    #     pos = {}
-    #     for v in range(1, m * n + 1):
-    #         pos[v] = ((v - 1) % n, (m * n - v) // n)
-    #     nx.draw_networkx_nodes(G, pos)
-    #     nx.draw_networkx_labels(G, pos)
-    #     nx.draw_networkx_edges(G, pos)
-    #     plt.xticks([])
-    #     plt.yticks([])
-    #     plt.show()
