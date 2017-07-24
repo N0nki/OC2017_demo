f2b3f2512471951df729e6c52fbef62908eeb6cb
N0nki <rgstr_land@icloud.com>
2017-07-24 19:26:42 +0900

    add function _get_node_pos and fix draw

--- lib/gridgraph.py
+++ lib/gridgraph.py
@@ -11,6 +11,7 @@ class GridGraph:
         self.m = m
         self.n = n
         self.nx_graph = self._grid_graph(m, n)
+        self.node_pos = self._get_node_pos()
 
     def _grid_graph(self, m, n):
         """
@@ -29,33 +30,35 @@ class GridGraph:
         G.add_weighted_edges_from(edges)
         return G
 
-    def draw(self, subgraph=None, edge_labels=None):
+    def _get_node_pos(self):
+        n = sorted(self.nx_graph[1].keys())[1] - 1
+        m = self.nx_graph.number_of_nodes() // self.n
+        pos = {}
+        for v in range(1, m * n + 1):
+            pos[v] = ((v - 1) % n, (m * n - v) // n)
+        return pos
+
+    def draw(self, subgraph=None, edge_labels=None, figsize=None):
         """
         格子グラフを描画する
         """
-        universe = GraphSet.universe()
-        if not isinstance(universe, nx.Graph):
-            universe = nx.Graph(data=list(universe))
-        self.n = sorted(universe[1].keys())[1] - 1
-        self.m = universe.number_of_nodes() // self.n
-        self.nx_graph.add_nodes_from(universe.nodes())
-        pos = {}
-        plt.figure(figsize=(self.m, self.n))
-        for v in range(1, self.m * self.n + 1):
-            pos[v] = ((v - 1) % self.n, (self.m * self.n - v) // self.n)
-        nx.draw_networkx_nodes(self.nx_graph, pos, node_color='w')
-        nx.draw_networkx_labels(self.nx_graph, pos)
-        nx.draw_networkx_edges(self.nx_graph, pos)
+        # if figsize:
+        #     plt.figure(figsize=figsize)
+        # else:
+        #     plt.figure(figsize=(self.m, self.n))
+        nx.draw_networkx_nodes(self.nx_graph, self.node_pos, node_color='w')
+        nx.draw_networkx_labels(self.nx_graph, self.node_pos)
+        nx.draw_networkx_edges(self.nx_graph, self.node_pos)
         if edge_labels:
-            nx.draw_networkx_edge_labels(self.nx_graph, pos, edge_labels=edge_labels, font_size=8)
+            nx.draw_networkx_edge_labels(self.nx_graph, self.node_pos, edge_labels=edge_labels, font_size=8)
         if subgraph:
             subgraph = nx.Graph(data=subgraph)
-            nx.draw_networkx_nodes(subgraph, pos, node_color='r')
-            # nx.draw_networkx_labels(subgraph, pos, font_size=10)
-            nx.draw_networkx_edges(subgraph, pos, edgelist=subgraph.edges(), edge_color='r', width=3.0)
+            nx.draw_networkx_nodes(subgraph, self.node_pos, node_color='r')
+            # nx.draw_networkx_labels(subgraph, self.node_pos, font_size=10)
+            nx.draw_networkx_edges(subgraph, self.node_pos, edgelist=subgraph.edges(), edge_color='r', width=3.0)
             if edge_labels:
                 subgraph_metric = {(e[0],e[1]): edge_labels[e] for e in subgraph.edges()}
-                nx.draw_networkx_edge_labels(subgraph, pos, edge_labels=subgraph_metric, font_color='r', font_size=8)
+                nx.draw_networkx_edge_labels(subgraph, self.node_pos, edge_labels=subgraph_metric, font_color='r', font_size=8)
         plt.xticks([])
         plt.yticks([])
-        plt.show()
+        # plt.show()
